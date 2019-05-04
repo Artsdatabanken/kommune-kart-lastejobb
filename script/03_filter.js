ca65bc6925b250ea9229c57befc9a96e1237a247
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-04 14:20:50 +0200

    Bruk autorkode som felles betegnelse både på kommune og fylke

--- script/03_filter.js
+++ script/03_filter.js
@@ -1,4 +1,4 @@
-const { io } = require("lastejobb");
+const { io, json } = require("lastejobb");
 
 map("Basisdata_0000_Norge_25833_Fylker_GEOJSON.geojson", "fylke");
 map("Basisdata_0000_Norge_25833_Kommuner_GEOJSON.geojson", "kommune");
@@ -22,6 +22,8 @@ function map(srcPath, navn) {
   geo.features.forEach(f => {
     const props = f.properties;
     scrub.forEach(key => delete props[key]);
+    json.moveKey(props, "fylkesnummer", "autorkode");
+    json.moveKey(props, "kommunenummer", "autorkode");
     props.navn = mapNavn(props.navn);
   });
   const dstPath = navn + ".geojson";
@@ -29,9 +31,20 @@ function map(srcPath, navn) {
 }
 
 function mapNavn(navn) {
+  // From: https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+  // To: https://iso639-3.sil.org/code_tables/639/data/s?page=1
+  const iso2to3 = {
+    fkv: "fkv", // Finsk kvensk
+    nor: "nob", // Norsk bokmål
+    sma: "sma", // Nord-samisk
+    sme: "sme", // Sør-samisk
+    smj: "smj" // Lule Sami
+  };
+
   return navn.reduce((acc, n) => {
-    // https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
-    acc[n.sprak] = n.navn;
+    const språkkode = iso2to3[n.sprak];
+    if (!språkkode) throw new Error("No ISO 639-3 code for: " + n.sprak);
+    acc[språkkode] = n.navn;
     return acc;
   }, {});
 }
