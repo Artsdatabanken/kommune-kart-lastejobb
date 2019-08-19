673aa5e7678d7be4d284cfc108e1391f1092ce62
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-20 01:53:50 +0200

    Deploy snippet

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+scp build/fylke_4326.geojson  grunnkart@hydra:~/tilesdata/Administrativ_grense/Territorialområde/Fastlands-Norge/fylke.4326.geojson
+scp build/kommune_4326.geojson  grunnkart@hydra:~/tilesdata/Administrativ_grense/Territorialområde/Fastlands-Norge/kommune.4326.geojson
+scp build/fylke_25833.geojson  grunnkart@hydra:~/tilesdata/Administrativ_grense/Territorialområde/Fastlands-Norge/fylke.25833.geojson
+scp build/kommune_25833.geojson  grunnkart@hydra:~/tilesdata/Administrativ_grense/Territorialområde/Fastlands-Norge/kommune.25833.geojson
+
