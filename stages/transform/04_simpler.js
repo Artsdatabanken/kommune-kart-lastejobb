00c4a12bb12dae2a0fa5ada016f3d76a7a20e3af
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-28 19:24:07 +0200

    layer name polygons

--- stages/transform/04_simpler.js
+++ stages/transform/04_simpler.js
@@ -8,7 +8,7 @@ simplify("kommune.geojson", "kommune_25833.geojson", 1);
 function simplify(src, target, tolerance) {
   const targetPath = "./data/" + target;
   if (fs.existsSync(targetPath)) fs.unlinkSync(targetPath);
-  const cmdline = `ogr2ogr -f GeoJSON -simplify ${tolerance} ${targetPath} data/${src}`;
+  const cmdline = `ogr2ogr -nln polygons -f GeoJSON -simplify ${tolerance} ${targetPath} data/${src}`;
   log.info("Running: " + cmdline);
   execSync(cmdline);
 }
--- stages/transform/05_reproject.js
+++ stages/transform/05_reproject.js
@@ -1,9 +1,10 @@
-const { io } = require("lastejobb");
 const execSync = require("child_process").execSync;
 
 reproject("fylke_25833.geojson", "fylke_4326.geojson");
 reproject("kommune_25833.geojson", "kommune_4326.geojson");
 
 function reproject(src, target, epsg = "EPSG:4326") {
-  execSync(`ogr2ogr -f GeoJSON -t_srs ${epsg} data/${target} data/${src}`);
+  execSync(
+    `ogr2ogr -nln polygons -f GeoJSON -t_srs ${epsg} data/${target} data/${src}`
+  );
 }
