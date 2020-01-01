bcbb5f0f94c085e13b2fea69c85b8a56ad71bb1d
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-01-01 06:04:25 +0100

    Både filnavn og format endret i kildedata
    Forespørsel om stabil url sendt kartverket

--- stages/download/01_download_kartverket_geometri.js
+++ stages/download/01_download_kartverket_geometri.js
@@ -2,10 +2,11 @@ const { http, io, log } = require("lastejobb");
 
 const downloads = {
   kommune:
-    "https://nedlasting.geonorge.no/geonorge/Basisdata/Kommuner/GeoJSON/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.zip",
-
+    "https://nedlasting.geonorge.no/geonorge/Basisdata/Kommuner2020/GeoJSON/Basisdata_0000_Norge_25833_Kommuner2020_GEOJSON.zip",
+  //    "https://nedlasting.geonorge.no/geonorge/Basisdata/Kommuner/GeoJSON/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.zip",
   fylke:
-    "https://nedlasting.geonorge.no/geonorge/Basisdata/Fylker/GeoJSON/Basisdata_0000_Norge_25833_Fylker_GEOJSON.zip"
+    "https://nedlasting.geonorge.no/geonorge/Basisdata/Fylker2020/GeoJSON/Basisdata_0000_Norge_25833_Fylker2020_GEOJSON.zip"
+  //    "https://nedlasting.geonorge.no/geonorge/Basisdata/Fylker/GeoJSON/Basisdata_0000_Norge_25833_Fylker_GEOJSON.zip"
 };
 
 Object.entries(downloads).forEach(([navn, url]) => {
