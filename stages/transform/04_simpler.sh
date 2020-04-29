8e92efcca9aa4e5620dd1da098c190addb545815
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-04-29 14:12:09 +0200

    Wrap ogr2ogr in a function

--- stages/transform/04_simpler.sh
+++ stages/transform/04_simpler.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-#simplify("fylke.geojson", "fylke_25833.geojson", 1);
-#simplify("kommune.geojson", "kommune_25833.geojson", 1);
 
-#docker run --rm -v /home:/home osgeo/gdal:alpine-small-latest ls -latr $PWD/temp/fylke_25833.geojson $PWD/temp/fylke.geojson
-docker run --rm -v /home:/home osgeo/gdal:alpine-small-latest ogr2ogr -nln polygons -f GeoJSON -simplify 1 $PWD/temp/fylke_25833.geojson $PWD/temp/fylke.geojson
-docker run --rm -v /home:/home osgeo/gdal:alpine-small-latest ogr2ogr -nln polygons -f GeoJSON -simplify 1 $PWD/temp/kommune_25833.geojson $PWD/temp/kommune.geojson
-
-
-ogr2ogr(args) {
-    echo $args
-    docker run --rm -v /home:/home osgeo/gdal:alpine-small-latest ogr2ogr $args
+ogr2ogr() {
+    echo $1
+    docker run --rm -v /home:/home osgeo/gdal:alpine-small-latest ogr2ogr $1
 }
+
+ogr2ogr "-nln polygons -f GeoJSON -simplify 1 $PWD/temp/fylke_25833.geojson $PWD/temp/fylke.geojson"
+ogr2ogr "-nln polygons -f GeoJSON -simplify 1 $PWD/temp/kommune_25833.geojson $PWD/temp/kommune.geojson"
