286ae97227954daf40aa6c2c5bd0ab2bef35e26f
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-04-29 14:35:51 +0200

    Fix: ERROR 6: GEOS support not enabled.

--- stages/transform/04_simpler.sh
+++ stages/transform/04_simpler.sh
@@ -4,7 +4,7 @@ set -e
 
 ogr2ogr() {
     echo $1
-    docker run --rm -v /home:/home osgeo/gdal:alpine-small-latest ogr2ogr $1
+    docker run --rm -v /home:/home osgeo/gdal:alpine-normal-latest ogr2ogr $@
 }
 
 ogr2ogr "-nln polygons -f GeoJSON -simplify 1 $PWD/temp/fylke_25833.geojson $PWD/temp/fylke.geojson"
